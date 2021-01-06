33b42b4c74b6d9b91ecd0c21ed144dd1ab994a3b
amistele <amistel@ncsu.edu>
2021-01-05 23:33:27 -0500

    Radio button functionality, initial settings, and manual control panel visibility

--- bin/ui_ctUtilsFinder.m
+++ bin/ui_ctUtilsFinder.m
@@ -20,7 +20,10 @@ function ui_ctUtilsFinder(bufferLocal,listString,fParent)
     bufferUpdated = struct('x',cell(1,length(bufferLocal)));   % AIRFOILS AFTER LE UPDATES
     
     % DEFINE DEFAULT SETTINGS
-    settings.plottedID = NaN;
+    settings.plottedID      = NaN;
+    settings.iterateFrom    = 1; % 1 for LE, 0 for TE
+    settings.distribute     = 1; % 1 for COSINE, 0 for LINEAR
+    settings.mode           = 1; % 1 for AUTOMATIC, 0 for MANUAL
     
     for ind = 1:length(bufferLocal)
         bufferLocal(ind).isBlunt = NaN;
@@ -52,28 +55,51 @@ function ui_ctUtilsFinder(bufferLocal,listString,fParent)
     height2 = 0.5  - 2*eOff;    % PANEL HEIGHT 2
 
     % TOP LEFT PANEL
-    uiLocal.panel_upperLeft   = uipanel(f1,'Position',[eOff 0.2+eOff+height2+2*eOff width height1]);
+    uiLocal.panel_upperLeft     = uipanel(f1,'Position',[eOff 0.2+eOff+height2+2*eOff width height1]);
     
     % CENTER LEFT PANEL
-    uiLocal.panel_centerLeft = uipanel(f1,'Position',[eOff 0.2+eOff width height2]);
+    uiLocal.panel_centerLeft    = uipanel(f1,'Position',[eOff 0.2+eOff width height2]);
     
     % LOWER LEFT PANEL
-    uiLocal.panel_lowerLeft  = uipanel(f1,'Position',[eOff eOff width height1]);
+    uiLocal.panel_lowerLeft     = uipanel(f1,'Position',[eOff eOff width height1]);
+    
+    % DIVIDER PANEL
+    uiLocal.panel_divider       = uipanel(f1,'Position',[eOff+width+eOff 0.05 0.01 0.9-0.1]);
+    
+    % MAIN RIGHT PANEL
+    uiLocal.panel_right         = uipanel(f1,'Position',[2*eOff+width+0.01+eOff eOff 1-eOff-(2*eOff+width+0.01+eOff) 0.9-2*eOff]);
+    
+    % SETTINGS PANEL
+    portion = 0.3;
+    uiLocal.panel_settings      = uipanel(uiLocal.panel_right,'Position',[eOff 1-portion+eOff 1-2*eOff portion-2*eOff]);
+    
+    % SOLVER PANEL
+    uiLocal.panel_solver        = uipanel(uiLocal.panel_right,'Position',[eOff eOff 1-2*eOff 1-portion-2*eOff]);
     
     
     % INPUT AIRFOILS LABEL
-    uiLocal.text_left    = uicontrol(uiLocal.panel_upperLeft,'Style','text','String','Input Airfoils',...
+    uiLocal.text_left       = uicontrol(uiLocal.panel_upperLeft,'Style','text','String','Input Airfoils',...
         'Units','normalized','Position',[0.1 0.8 0.8 0.2],...
         'Fontweight','bold','FontSize',12,'FontName',font);
     
     % FIND LE LABEL
-    uiLocal.text_center  = uicontrol(uiLocal.panel_centerLeft,'Style','text','String','LE Finder',...
+    uiLocal.text_center     = uicontrol(uiLocal.panel_centerLeft,'Style','text','String','LE Finder',...
         'Units','normalized','Position',[0.3 0.9 0.4 0.1],...
         'Fontweight','bold','FontSize',12,'FontName',font);
     
     % UPDATED AIRFOILS LABEL
-    uiLocal.text_right   = uicontrol(uiLocal.panel_lowerLeft,'Style','text','String','Updated Airfoils',...
+    uiLocal.text_right      = uicontrol(uiLocal.panel_lowerLeft,'Style','text','String','Updated Airfoils',...
         'Units','normalized','Position',[0.1 0.8 0.8 0.2],...
+        'Fontweight','bold','FontSize',12,'FontName',font);
+    
+    % SOLVER SETTINGS LABEL
+    uiLocal.text_settings   = uicontrol(uiLocal.panel_settings,'Style','text','String','Solver Settings',...
+        'Units','normalized','Position',[0.3 0.8 0.4 0.2],...
+        'Fontweight','bold','FontSize',12,'FontName',font);
+    
+    % SOLVER LABEL
+    uiLocal.text_solver     = uicontrol(uiLocal.panel_solver,'Style','text','String','Solver',...
+        'Units','normalized','Position',[0.3 0.9 0.4 0.1],...
         'Fontweight','bold','FontSize',12,'FontName',font);
     
     
@@ -157,13 +183,188 @@ function ui_ctUtilsFinder(bufferLocal,listString,fParent)
     uiLocal.button_SENDFOIL = uicontrol(uiLocal.panel_lowerLeft,'Style','pushbutton',...
         'String','Send to Cam/Thick Finder','Units','normalized','Position',[2*eOff eOff 1-4*eOff 0.2],...
         'Fontweight','bold','FontSize',10,'FontName',font);
+
+    
+%% MAIN MENU BUTTONS - RIGHT PANEL
+
+    % TITLE - 'ITERATE FROM' SETTING
+    uiLocal.title_ITERATE   = uicontrol(uiLocal.panel_settings,'Style','text','String','Iterate from:',...
+        'Units','normalized','position',[eOff 0.75 0.2-2*eOff 0.125],...
+        'HorizontalAlignment','left',...
+        'Fontweight','bold','Fontsize',10,'FontName',font);
+    
+    % TITLE - 'SPACING' SETTING
+    uiLocal.title_SPACING   = uicontrol(uiLocal.panel_settings,'Style','text','String','Distribution spacing:',...
+        'Units','normalized','position',[eOff 0.325 0.2-2*eOff 0.125],...
+        'HorizontalAlignment','left',...
+        'Fontweight','bold','Fontsize',10,'FontName',font);
+    
+    % TITLE - 'STEPPING' SETTING
+    uiLocal.title_STEPPING  = uicontrol(uiLocal.panel_settings,'Style','text','String','Stepping mode',...
+        'Units','normalized','position',[eOff+0.2 0.75 0.2-2*eOff 0.125],...
+        'HorizontalAlignment','left',...
+        'Fontweight','bold','Fontsize',10,'FontName',font);
+    
+    % TITLE - 'MISCELLANEOUS' SETTINGS
+    uiLocal.title_MISC      = uicontrol(uiLocal.panel_settings,'Style','text','String','Miscellaneous',...
+        'Units','normalized','position',[eOff+0.4 0.75 0.25-2*eOff 0.125],...
+        'HorizontalAlignment','center',...
+        'Fontweight','bold','Fontsize',10,'FontName',font);
+    
+    % TITLE - MANUAL SETTINGS
+    uiLocal.title_MANUAL    = uicontrol(uiLocal.panel_settings,'Style','text','String','Manual Control: Camber Guessing',...
+        'Units','normalized','position',[0.655 0.75 0.345-eOff 0.125],...
+        'HorizontalAlignment','center',...
+        'Fontweight','bold','Fontsize',10,'FontName',font);
+    
+    % TITLE - MANUAL SETTINGS DISABLED
+    uiLocal.title_DISABLED  = uicontrol(uiLocal.panel_settings,'Style','text','String','(disabled)',...
+        'Units','normalized','position',[0.655 0.5 0.345-eOff 0.125],...
+        'HorizontalAlignment','center',...
+        'Fontsize',10,'FontName',font);
+    
+    
+    % RADIO BUTTON - 'ITERATE FROM LE'
+    uiLocal.radio_LE        = uicontrol(uiLocal.panel_settings,'Style','radiobutton',...
+        'String','LE (recommended)','Units','normalized','Value',1,...
+        'Position',[2*eOff 0.5875+0.025 0.2-4*eOff 0.1625-eOff],...
+        'FontSize',10,'FontName',font);
+    
+    % RADIO BUTTON - 'ITERATE FROM TE'
+    uiLocal.radio_TE        = uicontrol(uiLocal.panel_settings,'Style','radiobutton',...
+        'String','TE','Units','normalized','Value',0,...
+        'Position',[2*eOff 0.425+eOff+0.025 0.2-4*eOff 0.1625-eOff],...
+        'FontSize',10,'FontName',font);
+    
+    
+    % RADIO BUTTON - 'COSINE'
+    uiLocal.radio_COSINE    = uicontrol(uiLocal.panel_settings,'Style','radiobutton',...
+        'String','cosine','Units','normalized','Value',1,...
+        'Position',[2*eOff 0.1625+0.025 0.2-4*eOff 0.1625-eOff],...
+        'FontSize',10,'FontName',font);
+    
+    % RADIO BUTTON - 'LINEAR'
+    uiLocal.radio_LINEAR    = uicontrol(uiLocal.panel_settings,'Style','radiobutton',...
+        'String','linear','Units','normalized','Value',0,...
+        'Position',[2*eOff eOff+0.025 0.2-4*eOff 0.1625-eOff],...
+        'FontSize',10,'FontName',font);
+    
+    
+    % RADIO BUTTON - 'AUTOMATIC'
+    uiLocal.radio_AUTOMATIC = uicontrol(uiLocal.panel_settings','Style','radiobutton',...
+        'String','automatic','Units','normalized','Value',1,...
+        'Position',[0.2+2*eOff 0.5875+0.025 0.2-4*eOff 0.1625-eOff],...
+        'FontSize',10,'FontName',font);
+    
+    % RADIO BUTTON - 'MANUAL'
+    uiLocal.radio_MANUAL    = uicontrol(uiLocal.panel_settings','Style','radiobutton',...
+        'String','manual','Units','normalized','Value',0,...
+        'Position',[0.2+2*eOff 0.425+0.025+eOff 0.2-4*eOff 0.1625-eOff],...
+        'FontSize',10,'FontName',font);
+    
+    
+    % LIST BOX - 'MISCELLANEOUS'
+    uiLocal.list_MISC       = uicontrol(uiLocal.panel_settings,'Style','list',...
+        'Units','normalized','position',[0.4+eOff 0.325 0.25-2*eOff 0.75-0.325-eOff],...
+        'Max',1,'Value',1,'String',{'# points: 50','step force: 1/5','tol: 1e-10'},...
+        'FontSize',10,'FontName',font);
+    
+    % EDIT BOX - EDIT MISCELLANEOUS VALUES
+    uiLocal.edit_MISC       = uicontrol(uiLocal.panel_settings,'Style','edit',...
+        'Units','normalized','position',[0.4+eOff 0.1625+eOff 0.25-2*eOff 0.1625-eOff],...
+        'String','50','FontSize',10,'FontName',font);
+    
+    % BUTTON - UPDATE MISCELLANEOUS VALUES
+    uiLocal.update_MISC     = uicontrol(uiLocal.panel_settings,'Style','pushbutton',...
+        'Units','normalized','position',[0.4+eOff eOff 0.25-2*eOff 0.1625-eOff],...
+        'String','Update','Fontweight','bold','FontSize',10,'FontName',font);
+    
+    
+%% MANUAL SETTINGS UI ELEMENTS
+    
+    % PANEL - MANUAL STEPPING
+    uiLocal.panel_manual    = uipanel(uiLocal.panel_settings,'Position',[0.655 eOff 1-0.655-eOff/2 0.75-eOff]);
+    
+    % TEXT - SLIDER LABEL TOP
+    uiLocal.text_SLIDER1    = uicontrol(uiLocal.panel_manual,'Style','text',...
+        'Units','normalized','position',[0 0.8 0.075 0.2],...
+        'String','1','FontSize',10,'Fontweight','bold','FontName',font);
+    
+    % TEXT - SLIDER LABEL BOTTOM
+    uiLocal.text_SLIDER0    = uicontrol(uiLocal.panel_manual,'Style','text',...
+        'Units','normalized','position',[0 0 0.075 0.2],...
+        'String','0','FontSize',10,'Fontweight','bold','FontName',font);
+    
+    % SLIDER - GUESS LOCATION
+    uiLocal.slider_GUESS    = uicontrol(uiLocal.panel_manual,'Style','slider',...
+        'Units','normalized','position',[0.075 eOff 0.1 1-2*eOff],...
+        'Min',0,'Max',1,'Value',0.25,'BackgroundColor',[1 1 1]);
+    
+    
+    % TEXT - Height %
+    uiLocal.text_HEIGHT     = uicontrol(uiLocal.panel_manual,'Style','text',...
+        'Units','normalized','position',[0.175+eOff 0.8 0.4 0.2],...
+        'String','Height %','Fontweight','bold','FontSize',10,'FontName',font);
+    
+    % EDIT - SLIDER VALUE DISPLAY
+    uiLocal.edit_HEIGHT     = uicontrol(uiLocal.panel_manual,'Style','edit',...
+        'Units','normalized','position',[0.175+eOff 0.65+eOff 0.4 0.2-2*eOff],...
+        'String',{'0.50'},'enable','inactive',...
+        'Fontsize',10,'FontName',font);
+    
+    % TEXT - 'rU - rL Error'
+    uiLocal.text_ERROR      = uicontrol(uiLocal.panel_manual,'Style','text',...
+        'Units','normalized','position',[0.175+eOff 0.4 0.4 0.2],...
+        'String','rU-rL Error','Fontweight','bold','FontSize',10,'FontName',font);
+    
+    % EDIT - ERROR DISPLAY
+    uiLocal.edit_ERROR      = uicontrol(uiLocal.panel_manual,'Style','edit',...
+        'Units','normalized','position',[0.175+eOff 0.25+eOff 0.4 0.2-2*eOff],...
+        'String',{'n/a'},'enable','inactive',...
+        'FontSize',10,'FontName',font);
+    
+    % BUTTON - SEEK
+    uiLocal.button_ITERATE  = uicontrol(uiLocal.panel_manual,'Style','pushbutton',...
+        'Units','normalized','position',[0.175+eOff eOff 0.4 0.2],...
+        'String','Seek','FontWeight','bold','FontSize',10,'FontName',font);
+    
+    % BUTTON - ACCEPT
+    uiLocal.button_ACCEPT   = uicontrol(uiLocal.panel_manual,'Style','pushbutton',...
+        'Units','normalized','position',[0.575+2*eOff 0.4 0.4 0.4],...
+        'String','Accept','FontWeight','bold','FontSize',12,'FontName',font);
+    
+    % BUTTON - HELP
+    uiLocal.button_HELP     = uicontrol(uiLocal.panel_manual,'Style','pushbutton',...
+        'Units','normalized','position',[0.575+2*eOff 0.15+eOff 0.4 0.2],...
+        'String','Help','FontWeight','bold','FontSize',10,'FontName',font);
+    
+    % SET SOLVER SETTINGS PANEL INACTIVE B/C NO AIRFOIL LOADED INITIALLY
+    %set(uiLocal.panel_settings,'visible','off');
+    
+    % SET MANUAL CONTROL PANEL INACTIVE B/C AUTO STEPPING DEFAULT
+    set(uiLocal.panel_manual,'visible','off');
+    
+    % SET SOLVER PANEL INACTIVE B/C NO AIRFOIL LOADED INITIALLY
+    %set(uiLocal.panel_solver,'visible','off');
+    
     
 %% CALLBACK FUNCTION ASSIGNMENTS
     
-    % CALLBACKS DEFINED LOCALLY
+    % CALLBACKS DEFINED LOCALLY - LEFT PANEL
     uiLocal.button_SENDLE.Callback      = @sendLE;
     uiLocal.button_SET_NEAR.Callback    = @setGeomLE;
     uiLocal.button_SET_CURVE.Callback   = @setThetaLE;
+    
+    % CALLBACKS DEFINED LOCALLY - RIGHT PANEL, RADIO BUTTONS
+        % RADIO BUTTON CALLBACKS ARE A BIT CONVOLUTED BECAUSE I DIDN'T USE
+        % THE UIBUTTONGROUP FEATURE, WHICH MAKES MANAGING THE BUTTONS
+        % EASIER BUT WON'T LET ME POSITION THEM RELATIVELY ON A PANEL
+    uiLocal.radio_LE.Callback           = @updateRadioLE;
+    uiLocal.radio_TE.Callback           = @updateRadioTE;
+    uiLocal.radio_COSINE.Callback       = @updateRadioCosine;
+    uiLocal.radio_LINEAR.Callback       = @updateRadioLinear;
+    uiLocal.radio_AUTOMATIC.Callback    = @updateRadioAutomatic;
+    uiLocal.radio_MANUAL.Callback       = @updateRadioManual;
 
 
 %% CALLBACK FUNCTION DEFINITIONS
@@ -262,6 +463,7 @@ function ui_ctUtilsFinder(bufferLocal,listString,fParent)
 
             % UPDATE LISTBOX FOR UDPATED AIRFOILS
             uiLocal.list_AIRFOILS_UPDATED.String{end+1} = [uiLocal.list_AIRFOILS.String{idx} ' (nearest LE)'];   
+            uiLocal.list_AIRFOILS_UPDATED.Value         = length(uiLocal.list_AIRFOILS_UPDATED.String);
         end
     end
 
@@ -344,10 +546,134 @@ function ui_ctUtilsFinder(bufferLocal,listString,fParent)
             bufferUpdated(end).y = bufferUpdated(end).yNew;
 
             % UPDATE LISTBOX FOR UDPATED AIRFOILS
-            uiLocal.list_AIRFOILS_UPDATED.String{end+1} = [uiLocal.list_AIRFOILS.String{idx} ' (curvature LE)'];   
+            uiLocal.list_AIRFOILS_UPDATED.String{end+1} = [uiLocal.list_AIRFOILS.String{idx} ' (curvature LE)'];
+            uiLocal.list_AIRFOILS_UPDATED.Value         = length(uiLocal.list_AIRFOILS_UPDATED.String);
         end
     end
     
+
+    % UPDATE RADIO BUTTONS - ITERATE FROM - ON LE CLICK
+    %   FILES - < NONE >
+    function updateRadioLE(src,event)
+        % IF RADIO IS ALREADY SET TO LE, KEEP BUTTON ON
+        if uiLocal.radio_TE.Value == 0
+            uiLocal.radio_LE.Value = 1;
+        
+        % IF RADIO IS NOT SET TO LE, UPDATE BOTH RADIO BUTTONS
+        else 
+            % UPDATE BUTTON VALUES
+            uiLocal.radio_LE.Value = 1;
+            uiLocal.radio_TE.Value = 0;
+            
+            % UPDATE SOLVER SETTINGS
+            settings.iterateFrom = 1;
+        end
+        
+    end
+    
+
+    % UPDATE RADIO BUTTONS - ITERATE FROM - ON TE CLICK
+    %   FILES - < NONE >
+    function updateRadioTE(src,event)
+        % IF RADIO IS ALREADY SET TO TE, KEEP BUTTON ON
+        if uiLocal.radio_LE.Value == 0
+            uiLocal.radio_TE.Value = 1;
+        
+        % IF RADIO IS NOT SET TO TE, UPDATE BOTH RADIO BUTTONS
+        else
+            % UPDATE BUTTON VALUES
+            uiLocal.radio_LE.Value = 0;
+            uiLocal.radio_TE.Value = 1;
+            
+            % UPDATE SOLVER SETTINGS
+            settings.iterateFrom = 0;
+        
+
+        end
+    end
+
+
+    % UPDATE RADIO BUTTONS - DISTRIBUTION - ON COSINE CLICK
+    %   FILES - < NONE >
+    function updateRadioCosine(src,event)
+        % IF RADIO IS ALREADY SET TO COSINE, KEEP BUTTON ON
+        if uiLocal.radio_LINEAR.Value == 0
+            uiLocal.radio_COSINE.Value = 1;
+            
+        % IF RADIO IS NOT SET TO COSINE, UPDATE BOTH RADIO BUTTONS
+        else
+            % UPDATE BUTTON VALUES
+            uiLocal.radio_COSINE.Value = 1;
+            uiLocal.radio_LINEAR.Value = 0;
+            
+            % UPDATE SOLVER SETTINGS
+            settings.distribute = 1;
+        end
+    end
+
+
+    % UPDATE RADIO BUTTONS - DISTRIBUTION - ON LINEAR CLICK
+    %   FILES - < NONE >
+    function updateRadioLinear(src,event)
+        % IF RADIO IS ALREADY SET TO LINEAR, KEEP BUTTON ON
+        if uiLocal.radio_COSINE.Value == 0
+            uiLocal.radio_LINEAR.Value = 1;
+        
+        % IF RADIO IS NOT SET TO LIENAR, UPDATE BOTH RADIO BUTTONS
+        else
+            % UPDATE BUTTON VALUES
+            uiLocal.radio_COSINE.Value = 0;
+            uiLocal.radio_LINEAR.Value = 1;
+            
+            % UPDATE SOLVER SETTINGS
+            settings.distribute = 0;
+        end
+    end
+
+
+    % UPDATE RADIO BUTTONS - STEPPING MODE - ON AUTOMATIC CLICK
+    %   FILES - < NONE >
+    function updateRadioAutomatic(src,event)
+        % IF RADIO IS ALREADY SET TO AUTOMATIC, KEEP BUTTON ON
+        if uiLocal.radio_MANUAL.Value == 0
+            uiLocal.radio_AUTOMATIC.Value = 1;
+            
+        % IF RADIO IS NOT SET TO AUTOMATIC, UPDATE BOTH RADIO BUTTONS
+        else
+            % UPDATE BUTTON VALUES
+            uiLocal.radio_AUTOMATIC.Value = 1;
+            uiLocal.radio_MANUAL.Value = 0;
+            
+            % UPDATE SOLVER SETTINGS
+            settings.mode = 1;
+            
+            % SET MANUAL CONTROL PANEL TO INVISIBLE
+            set(uiLocal.panel_manual,'Visible','off');
+        end
+    end
+
+    
+    % UPDATE RADIO BUTTONS - STEPPING MODE - ON MANUAL CLICK
+    %   FILES - < NONE >
+    function updateRadioManual(src,event)
+        % IF RADIO IS ALREADY SET TO MANUAL, KEEP BUTTON ON
+        if uiLocal.radio_AUTOMATIC.Value == 0
+            uiLocal.radio_MANUAL.Value = 1;
+            
+        % IF RADIO IS NOT SET TO MANUAL, UPDATE BOTH RADIO BUTTONS
+        else
+            % UPDATE BUTTON VALUES
+            uiLocal.radio_AUTOMATIC.Value = 0;
+            uiLocal.radio_MANUAL.Value = 1;
+            
+            % UPDATE SOLVER SETTINGS
+            settings.mode = 0;
+            
+            % SET MANUAL CONTROL PANEL TO VISIBLE
+            set(uiLocal.panel_manual,'Visible','on');
+        end
+    end
+
 
 %% OTHER FUNCTIONS (NOT CALLBACKS) DEFINITIONS
     
@@ -378,6 +704,7 @@ function ui_ctUtilsFinder(bufferLocal,listString,fParent)
         
     end
 
+
     % DETERMINE TE POINT
     function [xTE, yTE, isBlunt] = locateTE(x,y)
         
@@ -399,6 +726,7 @@ function ui_ctUtilsFinder(bufferLocal,listString,fParent)
             yTE = y(1);
         end
     end
+
 
     % UPDATE PLOT WITH NEW POINTS CALCULATED
     function updatePlot(x,y,idx)
